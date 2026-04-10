f52664adbde0e47e75459e738d6936eba9761841
CristianComellas21 <ccf200@hotmail.com>
2026-04-10 16:52:35 +0200

    Add prettier and linter and configure it

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import astro from 'eslint-plugin-astro';
+
+export default [
+	...astro.configs.recommended,
+	...astro.configs['jsx-a11y-recommended'],
+	{
+		ignores: ['dist/', '.astro/', 'node_modules/'],
+	},
+];
---
+++ prettier.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: 'all',
+  printWidth: 100,
+};
