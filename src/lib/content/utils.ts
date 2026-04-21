051d258b8cd8d4b1909085c9d5d37335e78467b8
CristianComellas21 <ccf200@hotmail.com>
2026-04-21 19:53:48 +0200

    Add utility function to parse collection IDs for localization

---
+++ src/lib/content/utils.ts
@@ -0,0 +1,11 @@
+// src/lib/content/utils.ts
+export function parseCollectionId(id: string) {
+  const [lang, ...rest] = id.split('/');
+
+  return {
+    lang,
+    localizedId: id,
+    unlocalizedId: rest.join('/'),
+    slug: rest.at(-1) ?? '',
+  };
+}
