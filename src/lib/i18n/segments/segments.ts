d2dcd07ba6e1901c20330f28509cf9352b96252e
CristianComellas21 <ccf200@hotmail.com>
2026-04-23 19:48:32 +0200

    Remove legal route segments from routeSegmentsByGroup

--- src/lib/i18n/segments/segments.ts
+++ src/lib/i18n/segments/segments.ts
@@ -1,7 +1,5 @@
 import { commonRouteSegments } from './common';
-import { legalRouteSegments } from './legal';
 
 export const routeSegmentsByGroup = {
   common: commonRouteSegments,
-  legal: legalRouteSegments,
 } as const;
