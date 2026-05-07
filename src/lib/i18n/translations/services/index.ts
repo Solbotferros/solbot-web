7c9f4818c258abc0666e846bdd205c934ea03c64
CristianComellas21 <ccf200@hotmail.com>
2026-05-07 21:08:10 +0200

    Add translations for related projects and all projects in services

--- src/lib/i18n/translations/services/index.ts
+++ src/lib/i18n/translations/services/index.ts
@@ -3,18 +3,24 @@ export const servicesTranslations = {
     services: {
       primaryCta: 'Pide tu presupuesto',
       secondaryCta: 'Ver proyectos relacionados',
+      relatedProjects: 'Trabajos de este tipo realizados',
+      allProjects: 'Ver todos los proyectos',
     },
   },
   ca: {
     services: {
       primaryCta: 'Demana el teu pressupost',
       secondaryCta: 'Veure projectes relacionats',
+      relatedProjects: "Projectes d'aquest tipus realitzats",
+      allProjects: 'Veure tots els projectes',
     },
   },
   en: {
     services: {
       primaryCta: 'Request a quote',
       secondaryCta: 'View related projects',
+      relatedProjects: 'Related projects we have completed',
+      allProjects: 'View all projects',
     },
   },
 } as const;
