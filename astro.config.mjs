18a93dc8ecb2611f9b515194e2d355d67dd5b586
CristianComellas21 <ccf200@hotmail.com>
2026-06-16 11:44:28 +0200

    Update site URL in astro.config.mjs to include 'www' for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://solbotferros.es',
+  site: 'https://www.solbotferros.es',
   i18n: {
     defaultLocale: defaultLang,
     locales: locales,
